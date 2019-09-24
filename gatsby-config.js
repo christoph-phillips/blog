4be08fce6681cd8a2659965fafd50850de91e39f
Chris Phillips <chris.is.phillips@gmail.com>
2019-09-24 12:11:00 +0100

    Fixed gatsby plugin order errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,51 +5,20 @@ module.exports = {
     author: `@christoph-phillips`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-netlify-cms`,
-    {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: `${__dirname}/static/assets`,
-          name: 'images',
-        },
-      },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `content`,
-          path: `${__dirname}/content`,
-        },
-      },
-      'gatsby-transformer-remark',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-     {
             resolve: `gatsby-source-filesystem`,
                 options: {
                     path: `${__dirname}/static/assets`,
                     name: 'images',
                 },
         },
+              {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `content`,
+          path: `${__dirname}/content`,
+        },
+      },
       {
             resolve: `gatsby-source-filesystem`,
                 options: {
@@ -64,6 +33,32 @@ module.exports = {
                     name: 'portfolio',
                 },
         },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify-cms`,
+
+      'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+     
         {
             resolve: `gatsby-transformer-remark`,
             options: {
